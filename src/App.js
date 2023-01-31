21af87634ac6eed7a593405dfbfef079327357fd
vaishnavi satbhai <satbhaivaishnavi6@gmail.com>
2023-01-31 15:17:19 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import About from "./components/About";
+import Home from "./components/Home";
+import NavBar from "./components/NavBar";
+import SocialLinks from "./components/SocialLinks";
+import Portfolio from "./components/Portfolio";
+import Experience from "./components/Experience";
+import Contact from "./components/Contact";
+function App() {
+  return (
+    <div >
+     <NavBar/>
+     <SocialLinks/>
+      <Home />
+      <About/>
+      <Portfolio/>
+      <Experience/>
+      <Contact/>
+      
+    
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/About.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+const About= () => {
+ return  (
+<div name="about" className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white">
+              <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+                            <div className="pb-8">
+                            <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+                                          About
+                                          </p>        </div> <div>
+      <p className="text-xl mt-18">
+       My name is<i> vaishnavi satbhai </i>. I am complite my
+       10th from <b>Indian school of scholars pulgaon.</b>
+        and i am complite my 12th from <b> RK jounior college
+        pulgaon </b>.Now I am currently studing in <b>prof
+        Ram meghe college of engineering and management
+       badnera.</b>
+       <br/>
+       <br/> </p>
+               <p className="text-xl mt-18"> I have complite
+               internship in <b>Android app devlopment</b> from
+               golden bird education and <b>web development </b>  
+                from mikrotic.
+               and<b></b>
+       </p> </div>
+ </div></div> );};
+export default About;
